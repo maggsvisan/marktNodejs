41dc1b7aac41a53d910aaaa8e7931cbb2ce3c39c
maggsvisan <evamargarita.villarreal@gmail.com>
2018-02-12 11:50:55 -0600

    Update Server

--- Server.js
+++ Server.js
@@ -3,8 +3,11 @@ var PythonShell = require('python-shell');
 var io = require('socket.io').listen(net);
 
 //var HOST = '127.0.0.1';
-var HOST = '192.168.62.43';
+var HOST = '192.168.65.19';
 var PORT = 8080;
+var test1=0;
+var test2=0;
+
 
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
@@ -13,6 +16,8 @@ net.createServer(function(sock) {
 
     // We have a connection - a socket object is assigned to the connection automatically
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+	test1= sock.remoteAddress;
+	test2= sock.remotePort;
 
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
@@ -31,7 +36,8 @@ net.createServer(function(sock) {
 
     // Add a 'close' event handler to this instance of socket
     sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+        //console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+	console.log('CLOSED: ' + test1 +' '+ test2);
     });
 
 }).listen(PORT, HOST);
